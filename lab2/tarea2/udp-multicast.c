a145ed47f87127e6b2b2ac9bcf144289597bf252
xqft <estefano.bargas@fing.edu.uy>
2024-08-26 10:55:25 -0300

    Update

--- lab2/tarea2/udp-multicast.c
+++ lab2/tarea2/udp-multicast.c
@@ -9,15 +9,16 @@
 
 #include "sys/log.h"
 
-#define LOG_MODULE "App"
+#define LOG_MODULE "Multicast"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  1
-#define UDP_PORT	5678
+#define WITH_SERVER_REPLY 1
+#define UDP_PORT 5678
 
-#define SEND_INTERVAL		  (10 * CLOCK_SECOND)
+#define SEND_INTERVAL (10 * CLOCK_SECOND)
 
-static struct simple_udp_connection udp_conn; //udp_conn saves info of the comunication
+static struct simple_udp_connection udp_conn; // udp_conn saves info of the comunication
+const int GROUP_NUM = 1;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(udp_process, "UDP multicast");
@@ -25,28 +26,25 @@ AUTOSTART_PROCESSES(&udp_process);
 /*---------------------------------------------------------------------------*/
 static void // When msg recived this function is excecuted
 udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+                const uip_ipaddr_t *sender_addr,
+                uint16_t sender_port,
+                const uip_ipaddr_t *receiver_addr,
+                uint16_t receiver_port,
+                const uint8_t *data,
+                uint16_t datalen)
 {
 
-	leds_toggle(LEDS_RED);
-  LOG_INFO("Received message '%.*s' from ", datalen, (char *) data);
+  leds_toggle(LEDS_RED);
+  LOG_INFO("Mensaje recibido '%.*s' de ", datalen, (char *)data);
   LOG_INFO_6ADDR(sender_addr);
-#if LLSEC802154_CONF_ENABLED
-  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
-#endif
-  LOG_INFO_("\n");
+  LOG_INFO_("\n\r");
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_process, ev, data)
 {
   static struct etimer periodic_timer;
   static char str[36];
-	static radio_value_t ch_num;
+  static radio_value_t ch_num;
   uip_ipaddr_t dest_ipaddr;
 
   PROCESS_BEGIN();
@@ -54,33 +52,33 @@ PROCESS_THREAD(udp_process, ev, data)
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_rx_callback);
 
-	/* Initialize NodeId identification */
-	node_id_init();
+  /* Initialize NodeId identification */
+  node_id_init();
 
-	NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL,&ch_num);
-	LOG_INFO("RF_CHANNEL: %d\n", ch_num);
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 10 + GROUP_NUM);
+  NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL, &ch_num);
+  LOG_INFO("RF_CHANNEL: %d\n\r", ch_num);
 
-  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL); //random time 0 to 10 s
-  while(1) {
+  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL); // random time 0 to 10 s
+  while (1)
+  {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     /* Generates multicast address */
     uip_create_linklocal_allnodes_mcast(&dest_ipaddr);
 
-    LOG_INFO("Sending multicast (IP: ");
-    LOG_INFO_6ADDR(&dest_ipaddr);   //all motes connect to this IP, IP of multicast
-    LOG_INFO_(")\n");
+    LOG_INFO("Enviando multicast a");
+    LOG_INFO_6ADDR(&dest_ipaddr); // all motes connect to this IP, IP of multicast
+    LOG_INFO_("\n\r");
 
-    snprintf(str, sizeof(str), "Hello everybody! I am node %x.", node_id);
+    snprintf(str, sizeof(str), "Hola todos! soy el nodo %x.", node_id);
     simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr); // send info to multicast IP
-		leds_toggle(LEDS_GREEN);
+    leds_toggle(LEDS_GREEN);
 
     /* Add some jitter */
-    etimer_set(&periodic_timer, SEND_INTERVAL
-      - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND))); //random time 9 to 11 s
+    etimer_set(&periodic_timer, SEND_INTERVAL - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND))); // random time 9 to 11 s
   }
 
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-
--- lab2/tarea2/udp-server.c
+++ lab2/tarea2/udp-server.c
@@ -8,44 +8,44 @@
 #include <inttypes.h>
 
 #include "sys/log.h"
-#define LOG_MODULE "App"
+
+#define LOG_MODULE "Server"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  1
-#define UDP_PORT	5678
+#define WITH_SERVER_REPLY 1
+#define UDP_PORT 5678
 
-
-static struct simple_udp_connection udp_conn; //udp_conn saves info of the comunication
+static struct simple_udp_connection udp_conn; // udp_conn saves info of the comunication
+const int GROUP_NUM = 1;
 
 /*---------------------------------------------------------------------------*/
-PROCESS(udp_process, "UDP multicast");
+PROCESS(udp_process, "UDP server");
 AUTOSTART_PROCESSES(&udp_process);
 /*---------------------------------------------------------------------------*/
 static void // When msg recived this function is excecuted
 udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+                const uip_ipaddr_t *sender_addr,
+                uint16_t sender_port,
+                const uip_ipaddr_t *receiver_addr,
+                uint16_t receiver_port,
+                const uint8_t *data,
+                uint16_t datalen)
 {
-	static char str[36];
-	leds_toggle(LEDS_RED);
- LOG_INFO("Received message '%.*s' from ", datalen, (char *) data);
+  static char str[36];
+  leds_toggle(LEDS_RED);
+  LOG_INFO("Mensaje recibido '%.*s' de ", datalen, (char *)data);
   LOG_INFO_6ADDR(sender_addr);
+  LOG_INFO_("\n\r");
 
-  LOG_INFO_("\n");
-  snprintf(str, sizeof(str), "MSJ recibido");
-  simple_udp_sendto(&udp_conn, str, strlen(str), sender_addr); // send info to multicast IP
-		leds_toggle(LEDS_GREEN);
+  snprintf(str, sizeof(str), "Recibi tu mensaje");
+  simple_udp_sendto(&udp_conn, str, strlen(str), sender_addr);
+  leds_toggle(LEDS_GREEN);
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_process, ev, data)
 {
-  
   static char str[40];
-	static radio_value_t ch_num;
+  static radio_value_t ch_num;
   uip_ipaddr_t dest_ipaddr;
 
   PROCESS_BEGIN();
@@ -53,27 +53,23 @@ PROCESS_THREAD(udp_process, ev, data)
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_rx_callback);
 
-	/* Initialize NodeId identification */
-	node_id_init();
+  /* Initialize NodeId identification */
+  node_id_init();
 
-	NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL,&ch_num);
-	LOG_INFO("RF_CHANNEL: %d\n", ch_num);
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 10 + GROUP_NUM);
+  NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL, &ch_num);
+  LOG_INFO("RF_CHANNEL: %d\n", ch_num);
+  /* Generates multicast address */
+  uip_create_linklocal_allnodes_mcast(&dest_ipaddr);
 
+  LOG_INFO("Enviando multicast a");
+  LOG_INFO_6ADDR(&dest_ipaddr); // all motes connect to this IP, IP of multicast
+  LOG_INFO_("\n");
 
-    /* Generates multicast address */
-    uip_create_linklocal_allnodes_mcast(&dest_ipaddr);
-
-    LOG_INFO("Sending multicast (IP: ");
-    LOG_INFO_6ADDR(&dest_ipaddr);   //all motes connect to this IP, IP of multicast
-    LOG_INFO_(")\n");
-
-    snprintf(str, sizeof(str), "Hola soy el servidor del grupo NUMERO");
-    simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr); // send info to multicast IP
-		leds_toggle(LEDS_GREEN);
-
-
+  snprintf(str, sizeof(str), "Hola soy el servidor del grupo %d", GROUP_NUM);
+  simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr); // send info to multicast IP
+  leds_toggle(LEDS_GREEN);
 
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-
