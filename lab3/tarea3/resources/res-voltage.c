29250ad89ad805cb6f4dda4005d363d9f8b8ef5c
xqft <estefano.bargas@fing.edu.uy>
2024-08-29 22:04:51 -0300

    Added tarea3

---
+++ lab3/tarea3/resources/res-voltage.c
@@ -0,0 +1,57 @@
+#include "contiki.h"
+
+#include <string.h>
+#include <stdio.h>
+#include "coap-engine.h"
+
+#define NUM_SAMPLES 5
+
+#ifdef CONTIKI_TARGET_CC26X0_CC13X0
+#pragma message("Utilizando el target cc26x0-cc13x0")
+#include "batmon-sensor.h"
+extern coap_resource_t res_batmon;
+#else
+#pragma message("Utilizando un target distinto al cc26x0-cc13x0")
+#endif
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+RESOURCE(res_voltage,
+         "title=\"Voltage\";rt=\"Voltage\"",
+         res_get_handler,
+         NULL,
+         NULL,
+         NULL);
+
+static void
+res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+    unsigned int accept = -1;
+    coap_get_header_accept(request, &accept);
+
+    if (accept == -1 || accept == TEXT_PLAIN)
+    {
+        // Initialize temp and voltage
+        volt = 0;
+
+        // Start measuring
+        SENSORS_ACTIVATE(batmon_sensor);
+        for (i = 0; i < NUM_SAMPLES; i++)
+        {
+            // IIR, alpha = 0.5
+            volt = (((batmon_sensor.value(BATMON_SENSOR_TYPE_VOLT) * 125) >> 5) + volt) / 2;
+        }
+        SENSORS_DEACTIVATE(batmon_sensor);
+
+        coap_set_header_content_format(response, TEXT_PLAIN);
+        snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "%d", volt);
+
+        coap_set_payload(response, (uint8_t *)buffer, strlen((char *)buffer));
+    }
+    else
+    {
+        coap_set_status_code(response, NOT_ACCEPTABLE_4_06);
+        const char *msg = "Supporting content-types text/plain";
+        coap_set_payload(response, msg, strlen(msg));
+    }
+}
